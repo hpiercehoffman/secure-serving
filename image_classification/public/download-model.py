b93d48c3d09601c8c93118231c0947d4f996c9e5
Hannah Pierce-Hoffman <hrp2112@columbia.edu>
2023-11-23 11:17:13 -0500

    clean up some redundant files

--- image_classification/public/download-model.py
+++
@@ -1,7 +0,0 @@
-import torch
-from torchvision import models
-
-model = models.resnet18(pretrained=True)
-torch.save(model, 'resnet18_pretrained.pth')
-
-print("Model saved successfully as 'resnet18_pretrained.pth'")
